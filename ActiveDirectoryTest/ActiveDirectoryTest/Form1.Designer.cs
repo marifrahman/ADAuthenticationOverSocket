7636b6bd6c1dbd81e58adb88459ca8b6b2f8d3b7
marahaman <sun2moon@gmail.com>
2013-07-08 10:44:59 +0930

    Added another project
    The added project is that I have used to get the SIDs that are actually
    used as a reference for each user on the web app and the corresponding
    user in AD.

---
+++ ActiveDirectoryTest/ActiveDirectoryTest/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace ActiveDirectoryTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(23, 12);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(199, 20);
+            this.txtUserName.TabIndex = 0;
+            this.txtUserName.Text = "arahaman";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(147, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Signin";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(23, 38);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.PasswordChar = '*';
+            this.txtPwd.Size = new System.Drawing.Size(199, 20);
+            this.txtPwd.TabIndex = 2;
+            this.txtPwd.Text = "Allah4all";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(14, 117);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(449, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(23, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 56);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "GetAllUsers";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(581, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(665, 1002);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1801, 1062);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtUserName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ ActiveDirectoryTest/ActiveDirectoryTest/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+
+using System.Runtime.InteropServices; 
+
+namespace ActiveDirectoryTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //        FYI, PrincipalContext implements IDisposable, so better remember to wrap this in a using clause. – Jeremy McGee Jul 22 '10 at 10:51
+        //12	
+
+        //In my domain, I had to specify pc.ValidateCredentials("myuser", "mypassword", ContextOptions.Negotiate) or I would get System.DirectoryServices.Protocols.DirectoryOperationException: The server cannot handle directory requests. – Alex Peck Jun 29 '11 at 14:14
+        //3	
+
+        //If a password's expired or the accounts disabled, then ValidateCredentials will return false. Unfortuantly, it doesn't tell you why it's returned false (which is a pity as it means I can't do something sensible like redirect the user to change their password). – Chris J Sep 8 '11 at 15:10
+        //22	upvote
+        //    flag
+        //Also beware the 'Guest' account -- if the domain-level Guest account is enabled, ValidateCredentials returns true if you give it a non-existant user. As a result, you may want to call UserPrinciple.FindByIdentity to see if the passed in user ID exists first. 
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //DirectoryEntry adsEntry = new DirectoryEntry("LDAP://DC=ad.local", txtUserName.Text, txtPwd.Text);
+            //DirectorySearcher adsSearcher = new DirectorySearcher(adsEntry);
+            ////adsSearcher.Filter = "(&(objectClass=user)(objectCategory=person))";
+            //adsSearcher.Filter = "(sAMAccountName=" + txtUserName.Text + ")";
+
+            //try
+            //{
+            //    SearchResult adsSearchResult = adsSearcher.FindOne();
+            //    //bSucceeded = true;
+
+            //    //strAuthenticatedBy = "Active Directory";
+            //    //strError = "User has been authenticated by Active Directory.";
+            //    label1.Text = "User has been authenticated by Active Directory.";
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    // Failed to authenticate. Most likely it is caused by unknown user
+            //    // id or bad strPassword.
+            //    label1.Text = ex.Message;
+
+            //}
+            //finally
+            //{
+            //    adsEntry.Close();
+            //}
+
+            using (PrincipalContext pc = new PrincipalContext(ContextType.Domain, "ad.local"))
+            {
+                // validate the credentials
+                //pc.UserPrinciple.FindByIdentity
+                UserPrincipal userprincipal = UserPrincipal.FindByIdentity(pc, txtUserName.Text);
+                if (userprincipal != null)
+                {
+                    bool isValid = pc.ValidateCredentials(txtUserName.Text, txtPwd.Text);
+
+                    if (isValid)
+                        textBox1.Text = userprincipal.Sid.ToString();
+                    else
+                        textBox1.Text = "Not Authenticated";
+
+
+                }
+                else
+                {
+                    textBox1.Text = "Din find the user";
+                }
+
+
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (var context = new PrincipalContext(ContextType.Domain, "ad.local"))
+            {
+                using (var searcher = new PrincipalSearcher(new UserPrincipal(context)))
+                {
+                    foreach (var result in searcher.FindAll())
+                    {
+                        DirectoryEntry de = result.GetUnderlyingObject() as DirectoryEntry;
+                        //Console.WriteLine("First Name: " + de.Properties["givenName"].Value);
+                        //Console.WriteLine("Last Name : " + de.Properties["sn"].Value);
+                        //Console.WriteLine("SAM account name   : " + de.Properties["samAccountName"].Value);
+                        //Console.WriteLine("User principal name: " + de.Properties["userPrincipalName"].Value);
+                        //Console.WriteLine();
+
+                        textBox2.Text += de.Properties["samAccountName"].Value + "," ;
+
+                        byte[] SID = (byte[])de.Properties["objectSid"][0];
+                        textBox2.Text += GetSidString(SID) + Environment.NewLine;
+
+                    }
+                }
+            }
+            
+
+        }
+
+        [DllImport("advapi32", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool ConvertSidToStringSid([MarshalAs(UnmanagedType.LPArray)] byte[] pSID, out IntPtr ptrSid);
+
+        public static string GetSidString(byte[] sid)
+        {
+            IntPtr ptrSid;
+            string sidString;
+            if (!ConvertSidToStringSid(sid, out ptrSid))
+                throw new System.ComponentModel.Win32Exception();
+            try
+            {
+                sidString = Marshal.PtrToStringAuto(ptrSid);
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(ptrSid);
+            }
+            return sidString;
+        }
+
+
+    }
+}
